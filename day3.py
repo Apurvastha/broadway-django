58c48d979e920c79b4131dc3365b02d42d9d845a
Reaper <69500690+Apurvastha@users.noreply.github.com>
2025-01-16 21:39:28 +0545

    Classes and obj

--- day3.py
+++ day3.py
@@ -1,9 +1,23 @@
-def print_diamond(n):
-   # Upper half
-   for i in range(n):
-       print(" " * (n-i-1) + "*" * (2*i+1))
-   # Lower half    
-   for i in range(n-2, -1, -1):
-       print(" " * (n-i-1) + "*" * (2*i+1))
-
-print_diamond(4)
+class Arithmetic():
+    a = 10
+    b = 20
+    
+    def add(self):
+        return self.a + self.b
+    
+    def multiply(self):
+        return self.a * self.b
+    
+    def subtraction(self):
+        return self.a - self.b
+    
+    def division(self):
+        return self.a / self.b
+    
+obj1 = Arithmetic()
+obj2 = Arithmetic()
+obj2.a = obj2.add()
+print(obj1.add())
+print(obj2.multiply())
+print(obj1.subtraction())
+print(obj1.division())
